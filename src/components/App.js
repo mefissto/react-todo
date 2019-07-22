6362339196b773980dcf9537c8985079d4a00a27
mefissto <mefissto.w@gmail.com>
2019-07-22 23:37:13 +0300

    updated todo list

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,17 @@ import SearchPanel from './SearchPanel';
 import TodoList from './TodoList';
 
 const App = () => {
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
   return (
     <div>
       <Header />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData} />
     </div>
   );
 };
